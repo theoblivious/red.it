cd8c3b178d0d0a00bb0c921f1066ca4707a4f200
theoblivious <hashimoto.kenn@gmail.com>
2014-11-03 15:17:41 -0500

    comments out portions of controller git push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,16 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    respond_with(@posts)
+    # respond_with(@posts)
   end
 
   def show
-    respond_with(@post)
+    # respond_with(@post)
   end
 
   def new
     @post = Post.new
-    respond_with(@post)
+    # respond_with(@post)
   end
 
   def edit
@@ -21,17 +21,17 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    respond_with(@post)
+    # respond_with(@post)
   end
 
   def update
     @post.update(post_params)
-    respond_with(@post)
+    # respond_with(@post)
   end
 
   def destroy
     @post.destroy
-    respond_with(@post)
+    # respond_with(@post)
   end
 
   private
